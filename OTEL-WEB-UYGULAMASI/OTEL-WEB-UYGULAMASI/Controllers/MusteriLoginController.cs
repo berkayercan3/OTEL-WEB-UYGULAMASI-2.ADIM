7d77cb2e32a5015297642e574368f9de7bbce620
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2023-06-03 23:22:01 +0300

    Musteri Login İşlemleri yapıldı
    Artık üyeliğini tamamlayan müstteriler login sayfasında üyelikleri kontrol edilerek giriş işlemini başarılı bir şekilde yapabilecekler

---
+++ OTEL-WEB-UYGULAMASI/OTEL-WEB-UYGULAMASI/Controllers/MusteriLoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OTEL_WEB_UYGULAMASI.Models.Siniflar;
+
+
+namespace OTEL_WEB_UYGULAMASI.Controllers
+{
+    public class MusteriLoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        private Context _context;
+        public MusteriLoginController()
+        {
+            _context = new Context();
+        }
+
+        // Diğer action metodları burada bulunur
+
+        // Giriş işlemi için action metodu
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            // Kullanıcıyı veritabanında arayın
+            var user = _context.Users.FirstOrDefault(u => u.username == username && u.Password == password);
+
+            if (user != null)
+            {
+                // Giriş başarılı
+                // Oturumu aç veya gerekli işlemleri yap
+                return Redirect("https://localhost:44313/Musteri/Index");
+            }
+            else
+            {
+                // Giriş başarısız
+                // Hata mesajı döndür veya giriş sayfasını tekrar göster
+                ViewBag.ErrorMessage = "Kullanıcı adı veya şifre yanlış";
+                return View("Index");
+            }
+        }
+    }
+}
